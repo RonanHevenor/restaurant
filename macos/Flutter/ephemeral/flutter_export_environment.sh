001d6de6222942aa2efed099dcbe0ecfcde1c709
Ronan <spacechickenrobot@gmail.com>
2025-03-22 11:55:46 -0400

    aaa

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\space\code\flutter-sdk\fluttersdk2\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\space\code\personal-projects\restaurant\restaurant"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\space\\code\\flutter-sdk\\fluttersdk2\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\space\\code\\personal-projects\\restaurant\\restaurant" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\space\\code\\flutter-sdk\\fluttersdk2\\flutter"
+  "PROJECT_DIR=C:\\Users\\space\\code\\personal-projects\\restaurant\\restaurant"
+  "FLUTTER_ROOT=C:\\Users\\space\\code\\flutter-sdk\\fluttersdk2\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\space\\code\\personal-projects\\restaurant\\restaurant\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\space\\code\\personal-projects\\restaurant\\restaurant"
+  "FLUTTER_TARGET=C:\\Users\\space\\code\\personal-projects\\restaurant\\restaurant\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\space\\code\\personal-projects\\restaurant\\restaurant\\.dart_tool\\package_config.json"
+)
